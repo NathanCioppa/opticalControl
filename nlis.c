58c1c5b652f6afeb7c2d4b3c3a21ffa15299edb6
NathanCioppa <nathan.cioppa@gmail.com>
2025-10-01 17:40:55 -0400

    initial

---
+++ nlis.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <string.h>
+
+int main() {
+
+	int sockFd = socket(AF_NETLINK, SOCK_RAW, NETLINK_KOBJECT_UEVENT);
+	if(sockFd < 0) {
+		printf("failed to create socket.\n");
+		return 1;
+	}
+
+	// man 7 netlink for info on this formatting.
+	struct sockaddr_nl addr;
+	memset(&addr, 0, sizeof(struct sockaddr_nl));
+	addr.nl_family = AF_NETLINK;
+	
+	if(bind(sockFd, (struct sockaddr *) &addr, sizeof(struct sockaddr_nl)) == -1) {
+		printf("bind failed.\n");
+		return 2;
+	}
+
+	return 0;
+
+}
