b04d951cb17a245d95dec6c3d69cb11d094e0e37
NathanCioppa <nathan.cioppa@gmail.com>
2025-10-09 23:30:22 -0400

    added simple inquiry command

--- inquiry.c
+++ inquiry.c
@@ -10,47 +10,53 @@
 #define DEVICE_FILE "/dev/sg0"
 #define SCSI_GENERIC_INTERFACE_ID 'S'
 #define MAX_SENSE_BUF_LEN 64
+#define ALLOC_LEN 36
+#define OP_CODE 0x12
 
 int main() {
 	int fd = open(DEVICE_FILE, O_RDONLY | O_NONBLOCK);
-	if(fd == -1) {
-		printf("failed to open file /dev/sg0\n");
+	if(fd < 0) {
+		printf("failed tp open /dev/sg0\n");
 		return 1;
 	}
 
-	// SCSI command descriptor block for TEST UNIT READY a 6 bytes, all zero bytes.
+	// the command descriptor block for INQUIRY
 	unsigned char cdb[CDB_SIZE];
 	memset(cdb, 0, CDB_SIZE);
-	
-	// Documentation for sg_io_hdr_t type (at least the best docs I could find):
-	// https://sg.danny.cz/sg/p/scsi-generic_v3.txt
-	sg_io_hdr_t hdr;
-	memset(&hdr, 0, sizeof(sg_io_hdr_t));
+	cdb[0] = OP_CODE;
+	cdb[4] = ALLOC_LEN;
 
 	unsigned char senseBuf[MAX_SENSE_BUF_LEN];
 	memset(senseBuf, 0, MAX_SENSE_BUF_LEN);
 
+	unsigned char dataBuf[ALLOC_LEN];
+	memset(dataBuf, 0, ALLOC_LEN);
+	dataBuf[0] = 1;
+
+	sg_io_hdr_t hdr;
+	memset(&hdr, 0, sizeof(sg_io_hdr_t));
+
 	hdr.interface_id = SCSI_GENERIC_INTERFACE_ID;
-	hdr.dxfer_direction = SG_DXFER_NONE;
+	hdr.cmdp = cdb;
 	hdr.cmd_len = CDB_SIZE;
 	hdr.mx_sb_len = MAX_SENSE_BUF_LEN;
-	hdr.cmdp = cdb;
 	hdr.sbp = senseBuf;
+	hdr.dxfer_direction = SG_DXFER_FROM_DEV;
+	hdr.dxferp = dataBuf;
+	hdr.dxfer_len = ALLOC_LEN;
 	hdr.timeout = 5000;
 
 	if(ioctl(fd, SG_IO, &hdr) == -1) {
-		close(fd);
-		printf("Error in ioctl()\n");
+		printf("failed\n");
 		return 2;
 	}
-
-	if(hdr.sb_len_wr == 0) {
-		printf("unit ready\n");
-	}
 	else {
-		printf("unit not ready\n");
+		for(int i=0; i<ALLOC_LEN; i++) {
+			printf("%d: %x\n", i, dataBuf[i]);
+		}
 	}
-
-	close(fd);
 	return 0;
+
 }
+
+
--- readtoc.c
+++ readtoc.c
@@ -6,32 +6,49 @@
 #include <scsi/sg.h>
 #include <string.h>
 
-#define CDB_SIZE 6
+#define CDB_SIZE 10
+#define iOPCODE 0
+#define OPCODE 0x43
+#define iFORMAT 1
+#define FORMAT 0x02
+#define iMAX_ALLOC_LEN1 7
+#define iMAX_ALLOC_LEN2 8
+#define MAX_ALLOC_LEN_HALF 0xff
+
 #define DEVICE_FILE "/dev/sg0"
 #define SCSI_GENERIC_INTERFACE_ID 'S'
 #define MAX_SENSE_BUF_LEN 64
 
 int main() {
-	int fd = open(DEVICE_FILE, O_RDONLY | O_NONBLOCK);
+	int fd = open(DEVICE_FILE, O_RDONLY);
 	if(fd == -1) {
 		printf("failed to open file /dev/sg0\n");
 		return 1;
 	}
 
-	// SCSI command descriptor block for TEST UNIT READY a 6 bytes, all zero bytes.
 	unsigned char cdb[CDB_SIZE];
 	memset(cdb, 0, CDB_SIZE);
+	cdb[iOPCODE] = OPCODE;
+	///cdb[iFORMAT] = FORMAT;
+	cdb[iMAX_ALLOC_LEN2] = 0x04;
+	//cdb[iMAX_ALLOC_LEN2] = MAX_ALLOC_LEN_HALF;
+
 	
 	// Documentation for sg_io_hdr_t type (at least the best docs I could find):
 	// https://sg.danny.cz/sg/p/scsi-generic_v3.txt
 	sg_io_hdr_t hdr;
 	memset(&hdr, 0, sizeof(sg_io_hdr_t));
 
+	char dxferp[256];
+	memset(dxferp, 0, 256);
+
 	unsigned char senseBuf[MAX_SENSE_BUF_LEN];
-	memset(senseBuf, 0, MAX_SENSE_BUF_LEN);
+	memset(&senseBuf, 0, MAX_SENSE_BUF_LEN);
 
 	hdr.interface_id = SCSI_GENERIC_INTERFACE_ID;
-	hdr.dxfer_direction = SG_DXFER_NONE;
+	hdr.dxfer_direction = SG_DXFER_FROM_DEV;
+	hdr.dxferp = dxferp;
+	hdr.dxfer_len = 256;
 	hdr.cmd_len = CDB_SIZE;
 	hdr.mx_sb_len = MAX_SENSE_BUF_LEN;
 	hdr.cmdp = cdb;
@@ -45,12 +62,20 @@ int main() {
 	}
 
 	if(hdr.sb_len_wr == 0) {
-		printf("unit ready\n");
+		printf("good\n");
+		for(int i = 0; i<hdr.resid; i++) {
+			putchar(dxferp[i]);
+		}
+		putchar('\n');
 	}
 	else {
-		printf("unit not ready\n");
+		printf("sb len: %d\n", hdr.sb_len_wr);
+		for(int i=0; i<hdr.sb_len_wr; i++) {
+			printf("%x\n", hdr.sbp[i]);
+		}
 	}
 
 	close(fd);
 	return 0;
 }
+
